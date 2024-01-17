9ef273c9e1e60243a6d2aacf32ae2548562c6cea
anandrajB <anand98.ar@gmail.com>
2024-01-17 12:33:29 +0530

    initial commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
+import logging
 
 # Create your views here.
+
+
+logging.basicConfig(
+    filename="myproject.log", format="%(asctime)s %(message)s", filemode="w"
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
 def index(request):
+    logger.info("this is a index page")
     return render(request, "index.html")
 
 
 def page1(request):
+    logger.info("moved to page1 from index page")
     return render(request, "page1.html")
